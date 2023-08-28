0c3822269dfef232a6a10e695466c2459fd923ed
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-08-28 12:29:17 +0800

    fix call-before-modify issue

--- src/main.py
+++ src/main.py
@@ -4,16 +4,50 @@ import json
 import time
 from datetime import datetime
 
-import Sync as s
+def import_sync_module():
+    try:
+        import Sync as s
+        return s
+    except ImportError:
+        print("Error: Unable to import the Sync module.")
+        sys.exit()
+
+def get_cmd_args():
+    try:
+        return sys.argv[2], int(sys.argv[3]), int(sys.argv[4])
+    except IndexError:
+        return None, 1, 1
 
 def main():
+    # Get the command line arguments
+    cmd, goback_value, goforward_days = get_cmd_args()
+
+    # If users want to modify the JSON file
+    if cmd == "-m":
+        print(f"Going to set goback_days = {goback_value}")
+        print(f"Going to set goforward_days = {goforward_days}")
+
+        show_action = input("Do you want to modify json file (notion_setting.json) ? Y/N:  ")
+        if show_action.lower() in ["y", "yes"]:
+            modify_json(goback_value, goforward_days)
+        else:
+            print("Not modifying the JSON file...")
+            sys.exit()
+
+    # If users want to print on terminal or redirect to file
     show_action = input("Do you want to redirect to file? Y/N:  ")
     print("\n")
-    if show_action in ["y", "yes"]:
+
+    # Import the Sync module
+    s = import_sync_module()
+
+    # If users want to redirect to file
+    if show_action.lower() in ["y", "yes"]:
         write_to_file()
+    # If users want to print on terminal
     else:
         print("Displaying output on the terminal...")
-        run_sync_notion_gcal(sys.argv)  # Call the synchronization function
+        run_sync_notion_gcal(s, sys.argv)
 
 
 def write_to_file():
@@ -39,29 +73,9 @@ def write_to_file():
         sys.stdout = temp  # Restore the default stdout
 
 
-def run_sync_notion_gcal(cmd):
+def run_sync_notion_gcal(s, cmd):
     #TODO: add a function to check the cmd is valid or not
-    start_time = time.time()
-    try:
-        if cmd[2] == "-m":
-            try:
-                goback_value = int(cmd[3])
-                goforward_days = int(cmd[4])
-                print(f"Going to set goback_days = {goback_value}")
-                print(f"Going to set goforward_days = {goforward_days}")
-            except IndexError:
-                goback_value = 1
-                goforward_days = 1
-                print("Default goback_days and goforward_days values are 1")
-            show_action = input("Do you want to modify json file (notion_setting.json) ? Y/N:  ")
-            if show_action in ["y", "yes"]:
-                modify_json(goback_value, goforward_days)
-            else:
-                print("Not modifying the JSON file...")
-                print("\n")
-                sys.exit()
-    except Exception as e:
-        print("Default Notion Setting")
+    start_time = time.time()  # start time
     # default: update from notion to google
     if len(cmd) == 1 or cmd[1] == "-nm" or cmd[1] == "--update-modified-on-notion":
         s.notion_to_gcal(action=0, updateEverything=True)
@@ -147,4 +161,5 @@ def modify_json(goback_value=1, goforward_days=1):
 
 
 if __name__ == "__main__":
+    s = None
     main()
