6fc17cbc6c0225bff49702d1f6411a99f5e47a90
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-05-28 11:13:45 +0800

    feat: user cli

--- src/main.py
+++ src/main.py
@@ -1,2 +1,41 @@
-# Use terminal to control back or forward day.
-# Use terimal to control sync notion and google calendar, or based on timestamp.
+import argparse
+from sync import sync
+from user_setting import update_notion_setting
+
+def main():
+    parser = argparse.ArgumentParser(description="Welcome to Notion-Google Calendar Sync CLI!")
+
+    # Adding optional arguments
+    parser.add_argument('-t', '--timestamp', nargs=2, type=int, help="Update Notion Task and Google Calendar By timestamp")
+    parser.add_argument('-g', '--google', nargs=2, type=int, help="Force: Update Notion Task From Google Calendar")
+    parser.add_argument('-n', '--notion', nargs=2, type=int, help="Force: Update Google Calendar From Notion Task")
+
+    args = parser.parse_args()
+
+    # Handling no arguments case
+    if not args.timestamp and not args.google and not args.notion and not args.user_command:
+        print("Running sync with no arguments")
+        # Implement your logic for syncing with no arguments here
+    
+    # Handling optional -t flag for syncing by timestamp
+    if args.timestamp:
+        print(f"Running sync with timestamp: {args.timestamp}")
+        update_notion_setting.update_date_range(args.timestamp[0],args.timestamp[1])
+        sync.synchronize_notion_and_google_calendar()
+   
+
+    # Handling optional -g flag for syncing from Google Calendar to Notion
+    if args.google:
+        print(f"Running sync with Google Calendar to Notion: {args.google}")
+        update_notion_setting.update_date_range(args.google[0],args.google[1])
+        sync.force_update_notion_tasks_by_google_event_and_ignore_time()
+    
+    
+    # Handling optional -n flag for syncing from Notion to Google Calendar
+    if args.notion:
+        print(f"Running sync with Notion to Google Calendar: {args.notion}")
+        update_notion_setting.update_date_range(args.notion[0],args.notion[1])
+        sync.force_update_google_event_by_notion_task_and_ignore_time()
+
+if __name__ == "__main__":
+    main()
---
+++ src/user_setting/update_notion_setting.py
@@ -0,0 +1,70 @@
+import json
+from pathlib import Path
+
+# Get the absolute path to the current directory
+CURRENT_DIR = Path(__file__).parent.resolve()
+
+# Construct the absolute file paths within the container
+NOTION_SETTINGS_PATH = (CURRENT_DIR / "../../token/notion_setting.json").resolve()
+
+
+def update_date_range(goback_value, goforward_days):
+    """Modify the goback_days and goforward_days in notion_setting.json"""
+    data = read_json()
+    data["goback_days"], data["goforward_days"] = goback_value, goforward_days
+    print(
+        f"Modified goback_days to {goback_value} and goforward_days to {goforward_days} in notion_setting.json"
+    )
+    with open(NOTION_SETTINGS_PATH, "w") as file:
+        json.dump(data, file, indent=2)
+        print("Updated notion_setting.json:")
+        print(json.dumps(data, indent=2))
+
+def update_page_property(page_property, page_property_value):
+    """Modify the page_property in notion_setting.json"""
+    data = read_json()
+    data["page_property"][0][page_property] = page_property_value
+    print(
+        f"Modified page_property: {page_property} as {page_property_value} in notion_setting.json"
+    )
+    with open(NOTION_SETTINGS_PATH, "w") as file:
+        json.dump(data, file, indent=2)
+        print("Updated notion_setting.json:")
+        print(json.dumps(data, indent=2))
+
+def update_notion_token(notion_token):
+    """Modify the notion_token in notion_setting.json"""
+    data = read_json()
+    data["notion_token"] = notion_token
+    print(
+        f"Modified notion_token to {notion_token} in notion_setting.json"
+    )
+    with open(NOTION_SETTINGS_PATH, "w") as file:
+        json.dump(data, file, indent=2)
+        print("Updated notion_setting.json:")
+        print(json.dumps(data, indent=2))
+
+def update_urlroot(urlroot):
+    """Modify the notion_database_id in notion_setting.json"""
+    data = read_json()
+    data["urlroot"] = urlroot
+    print(
+        f"Modified urlroot to {urlroot} in notion_setting.json"
+    )
+    with open(NOTION_SETTINGS_PATH, "w") as file:
+        json.dump(data, file, indent=2)
+        print("Updated notion_setting.json:")
+        print(json.dumps(data, indent=2))
+
+def read_json():
+    with open(NOTION_SETTINGS_PATH, "r") as file:
+        data = json.load(file)
+        print("Current notion_setting.json:")
+        print(json.dumps(data, indent=2))
+    return data
+    
+
+if __name__ == "__main__":
+    # update_date_range(0,1)
+    # update_page_property("Task_Notion_Name", "YOUR NEW TASK NAME HERE")
+    print("You can update the notion_setting.json file by calling the functions in update_notion_setting.py")
