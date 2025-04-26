400ab51147f93c113e98a8d91fd993dccb56aff3
HUIXIN-TW <huixin.yang.tw@gmail.com>
2025-04-26 16:06:28 +0800

    feat: add api key verification

--- lambda_function.py
+++ lambda_function.py
@@ -9,12 +9,21 @@ sys.path.append(os.path.join(os.getcwd(), "src"))
 
 from src.main import main as run_sync_notion_and_google  # noqa: E402
 
+# API Key
+EXPECTED_API_KEY = os.environ.get("API_KEY")
+
 
 def lambda_handler(event, context):
     """
     AWS Lambda Handler to always run the CLI without any parameters and include Perth timezone information.
     """
     try:
+        # API Key check
+        headers = event.get("headers", {})
+        received_api_key = headers.get("x-api-key")
+
+        if received_api_key != EXPECTED_API_KEY:
+            return {"statusCode": 403, "body": json.dumps({"error": "Forbidden: Invalid API Key"})}
         # Get the current date, time, and timezone in UTC
         now_utc = datetime.now(timezone.utc)
         utc_date = now_utc.strftime("%Y-%m-%d")
@@ -55,6 +64,6 @@ def lambda_handler(event, context):
 
 # Mock event and context for local testing
 if __name__ == "__main__":
-    mock_event = {}
+    mock_event = {"headers": {"x-api-key": EXPECTED_API_KEY}}
     mock_context = {}
     print(lambda_handler(mock_event, mock_context))
