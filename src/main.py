446bb5e77bac525b0e000da1c2ea1cb91ef2f591
HUIXIN-TW <huixin.yang.tw@gmail.com>
2025-12-04 23:15:15 +0800

    style: format

--- src/main.py
+++ src/main.py
@@ -61,7 +61,7 @@ def main(uuid: str | None = None) -> dict:
         logger.debug(f"Using UUID: {uuid}")
 
         # Configure paths based on UUID (local vs dynamodb)
-        config = generate_config(uuid) # only return uuid in serverless mode
+        config = generate_config(uuid)  # only return uuid in serverless mode
 
         # Notion
         notion_config = NotionConfig(config, logger).get()
